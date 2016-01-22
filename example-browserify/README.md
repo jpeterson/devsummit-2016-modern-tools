09ed85106947866e86fc8b1c70d19d16b525e77a
Josh Peterson <jpeterson05@gmail.com>
2016-01-22 11:59:55 -0700

    add examples for webpack and npm

---
+++ example-browserify/README.md
@@ -0,0 +1,6 @@
+This example demonstrates 2 approaches to using Browserify to build and package your ES2015 app.
+
+
+via Grunt: `grunt`
+
+via NPM Scripts: `npm run dev`
--- example-browserify/gruntfile.js
+++ example-browserify/gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     'browserify': {
       dist: {
         files: {
-          'dist/app.js': ['src/index.js']
+          'dist/bundle.js': ['src/index.js']
         },
         options: {
           watch: true,
